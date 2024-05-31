52c0c48886ba82f6ccc0f3b5fecb5beef7e7b4da
Smriti Singh <smritisingh.its@gmail.com>
2024-05-31 09:14:35 +0530

    changed code from python to java

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,15 +1,26 @@
-def bubble_sort(arr):
-    n = len(arr)
-    
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                # Swap arr[j] and arr[j + 1]
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
 
-arr = [5, 2, 8, 12, 1, 6]
-bubble_sort(arr)
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 
-print("Sorted array:")
-for num in arr:
-    print(num, end=" ")
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6};
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
